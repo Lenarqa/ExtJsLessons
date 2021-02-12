9b95f75ac1077bbd2f1c5d4c537458a923495632
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2021-02-12 22:18:32 +0700

    panel with panels and formPanel

--- js/sait.js
+++ js/sait.js
@@ -9,17 +9,17 @@ Ext.onReady(function(){
     // });
 
     // Контейнер с компонентами
-    var component1 = Ext.create('Ext.Component', {
-        html: 'Component1'
-    });
+    // var component1 = Ext.create('Ext.Component', {
+    //     html: 'Component1'
+    // });
     
-    var component2 = Ext.create('Ext.Component', {
-        html: 'Component2'
-    });
+    // var component2 = Ext.create('Ext.Component', {
+    //     html: 'Component2'
+    // });
     
-    var component3 = Ext.create('Ext.Component', {
-        html: 'Component3'
-    });
+    // var component3 = Ext.create('Ext.Component', {
+    //     html: 'Component3'
+    // });
     
     // Ext.create('Ext.container.Container', {
     //     renderTo: Ext.getBody(),
@@ -31,14 +31,51 @@ Ext.onReady(function(){
     // });
     
     // Контейнер с контейнером
-    var container1 = Ext.create('Ext.container.Container', {
-        items: [component1, component2, component3]
-    });
+    // var container1 = Ext.create('Ext.container.Container', {
+    //     items: [component1, component2, component3]
+    // });
     
-    Ext.create('Ext.container.Container', {
-        renderTo: Ext.getBody(),
-        items: container1,
+    // Ext.create('Ext.container.Container', {
+    //     renderTo: Ext.getBody(),
+    //     items: container1,
+    // });
+
+    // Panel with panels
+    // var childPanel1 = Ext.create('Ext.Panel', {
+    //     html: 'child Panel 1'
+    // });
+
+    // var childPanel2 = Ext.create('Ext.Panel', {
+    //     html: 'child Panel 1'
+    // });
+
+    // Ext.create('Ext.panel.Panel', {
+    //     renderTo: Ext.getBody(),
+    //     width: 100,
+    //     height: 100,
+    //     border: true,
+    //     frame: true,
+    //     items: [childPanel1, childPanel2]
+    // });
+
+    // form Panel with panels
+    var textPanel1 = Ext.create('Ext.Panel', {
+        html: 'text Panel 1'
     });
+
+    var textPanel2 = Ext.create('Ext.Panel', {
+        html: 'text Panel 2'
+    });
+
+    Ext.create('Ext.form.Panel', {
+        renderTo: Ext.getBody(),
+        width: 100,
+        height : 100,
+        border : true,
+        frame : true,
+        layout : 'auto', 
+        items: [textPanel1, textPanel2]
+    })
 
 }); //Ext.onReady
 
