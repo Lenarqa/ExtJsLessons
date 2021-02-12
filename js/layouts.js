8955731501f601873f3fbf832ad34b1cbca795b8
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2021-02-13 02:13:38 +0700

    column, fit, vbox, hbox

--- js/layouts.js
+++ js/layouts.js
@@ -162,28 +162,224 @@
 // }); 
 
 // layout tab-panel этот макет позволяет позиционировать элементы, используя координаты XY в контейнере.
-Ext.onReady(function() {
-    Ext.create('Ext.tab.Panel', {
-        renderTo: Ext.getBody(),
-        requires: ['Ext.layout.container.Card'],
-        xtype: 'layout-cardtabs',
-        width: '100%',
-        height: 200,
+// Ext.onReady(function() {
+//     Ext.create('Ext.tab.Panel', {
+//         renderTo: Ext.getBody(),
+//         requires: ['Ext.layout.container.Card'],
+//         xtype: 'layout-cardtabs',
+//         width: '100%',
+//         height: 200,
         
+//         items: [
+//             {
+//                 title: 'Tab 1',
+//                 html: 'html tab 1',
+//             },
+//             {
+//                 title: 'Tab 2',
+//                 html: 'html tab 2',
+//             },
+//             {
+//                 title: 'Tab 3',
+//                 html: 'html tab 3',
+//             },
+           
+//         ]
+//     });
+// }); 
+
+// layout card Перелистывание карточек, есть кнопки для пролистывания
+// Ext.onReady(function() {
+//     Ext.create('Ext.panel.Panel', {
+//         renderTo: Ext.getBody(),
+//         requires: ['Ext.layout.container.Card'],
+//         layout: 'card',
+//         width: "100%",
+//         height: 200,
+//         bodyPadding: 15,
+        
+//         defaults: {
+//             border:false
+//         },
+
+//         defaultListenerScope: true,
+
+//         bbar: ['->', //Convenience config. Short for 'Bottom Bar'.
+//                {
+//                   itemId: 'card-prev',
+//                   text: '« Предыдущая карточка',
+//                   handler: 'showPrevious',
+//                   disabled: true
+//                },{
+//                   itemId: 'card-next',
+//                   text: 'Следующая карточка »',
+//                   handler: 'showNext'
+//                }],
+        
+//         items: [
+//             {
+//                 id: 'card0',
+//                 html: 'html карточка №1',
+//             },
+//             {
+//                 id: 'card1',
+//                 html: 'html карточка №2',
+//             },
+//             {
+//                 id: 'card2',
+//                 html: 'html карточка №3',
+//             },
+//         ],
+
+//         //функции для кнопок
+//         showNext: function () {
+//             this.cardPanelNavigation(1);
+//          },
+      
+//          showPrevious: function (btn) {
+//             this.cardPanelNavigation(-1);
+//          },
+
+//          cardPanelNavigation: function (incr) {
+//             var me = this;
+//             var l = me.getLayout();
+//             var i = l.activeItem.id.split('card')[1];
+//             var next = parseInt(i, 10) + incr;
+//             l.setActiveItem(next);
+//             me.down('#card-prev').setDisabled(next===0);
+//             me.down('#card-next').setDisabled(next===2);
+//          }
+//     });
+// }); 
+
+// layout column
+// Ext.onReady(function() {
+//     Ext.create('Ext.panel.Panel', {
+//         renderTo: Ext.getBody(),
+//         layout: 'column',
+//         xtype: 'layout-column',
+//         requires: ['Ext.layout.container.Column'],
+//         width: '100vw',
+//         border: false,
+
+//         items: [
+//             {
+//                 title: 'Колонка 1',
+//                 html: 'html Колонка №1',
+//                 columnWidth: 0.2
+//             },
+//             {
+//                 title: 'Колонка 2',
+//                 html: 'html Колонка №2',
+//                 columnWidth: 0.3
+//             },
+//             {
+//                 title: 'Колонка 3',
+//                 html: 'html Колонка №3',
+//                 columnWidth: 0.5
+//             },
+//         ],
+//     });
+// }); 
+
+
+// layout fit заполняет все свободное пространство
+// Ext.onReady(function() {
+//     Ext.create('Ext.container.Container', {
+//         renderTo: Ext.getBody(),
+//         layout: 'fit',
+//         width: 500,
+//         defaults: {
+//             bodyPadding: 15
+//         },
+
+//         items: [
+//             {
+//                 title: 'Панель 1',
+//                 html: 'html Панель №1',
+//             },
+//             {
+//                 title: 'Панель 2',
+//                 html: 'html Панель №2',
+//             },
+//             {
+//                 title: 'Панель 3',
+//                 html: 'html Панель №3',
+//             },
+//         ],
+//     });
+// }); 
+
+// layout vbox макет позволяет размещать элементы вертикально.
+// Ext.onReady(function() {
+//     Ext.create('Ext.panel.Panel', {
+//         renderTo: Ext.getBody(),
+//         layout: {
+//             type: 'vbox',
+//             align: 'stretch'
+//         },
+//         requires: ['Ext.layout.container.VBox'],
+//         xtype: 'layout-vertical-box',
+//         width: '100%',
+//         height: 400,
+//         frame :true,
+
+//         items: [
+//             {
+//                 title: 'Панель 1',
+//                 html: 'html Панель №1',
+//                 margin: '0 0 10 0',
+//                 flex: 1,
+//             },
+//             {
+//                 title: 'Панель 2',
+//                 html: 'html Панель №2',
+//                 margin: '0 0 10 0',
+//                 flex: 2,
+//             },
+//             {
+//                 title: 'Панель 3',
+//                 html: 'html Панель №3',
+//                 margin: '0 0 10 0',
+//                 flex: 1,
+//             },
+//         ],
+//     });
+// }); 
+
+// layout hBox размещает элементы горизонтально
+Ext.onReady(function() {
+    Ext.create('Ext.panel.Panel', {
+        renderTo: Ext.getBody(),
+        layout: {
+            type: 'hbox',
+            align: 'stretch'
+        },
+        requires: ['Ext.layout.container.HBox'],
+        xtype: 'layout-horizontal-box',
+        width: '100%',
+        height: 400,
+        // frame :true,
+
         items: [
             {
-                title: 'Tab 1',
-                html: 'html tab 1',
+                title: 'Панель 1',
+                html: 'html Панель №1',
+                margin: '0 2 0 2',
+                flex: 1,
             },
             {
-                title: 'Tab 2',
-                html: 'html tab 2',
+                title: 'Панель 2',
+                html: 'html Панель №2',
+                margin: '0 2 0 2',
+                flex: 2,
             },
             {
-                title: 'Tab 3',
-                html: 'html tab 3',
+                title: 'Панель 3',
+                html: 'html Панель №3',
+                margin: '0 2 0 2',
+                flex: 1,
             },
-           
-        ]
+        ],
     });
 }); 
